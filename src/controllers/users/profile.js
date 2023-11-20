47025de59fcbce3f3e7e4762c30256872599f4e5
MauroSuarez1607 <maurosuarez1994@gmail.com>
2023-11-19 23:44:42 -0300

    add js front profile

--- src/controllers/users/profile.js
+++ src/controllers/users/profile.js
@@ -3,10 +3,10 @@ const db = require('../../database/models')
 module.exports = (req,res) => {
     db.User.findByPk(req.session.userLogin.id)
     .then(user => {
-        const birthday = new Date(user.birthday).toISOString()
+        const birthday = user.birthday ? new Date(user.birthday).toISOString() : null
         return res.render('profile', {
             ...user.dataValues,
-            birthday : birthday.split('T')[0]
+            birthday : birthday ? birthday.split('T')[0] : null
         })
     })
     .catch(error => console.log(error))
