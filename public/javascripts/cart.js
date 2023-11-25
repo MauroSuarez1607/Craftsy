7d086cea469d09b66d6a0ec4f341e5f3573bbe69
MauroSuarez1607 <maurosuarez1994@gmail.com>
2023-11-24 21:00:15 -0300

    add view cart & function

--- public/javascripts/cart.js
+++ public/javascripts/cart.js
@@ -1,12 +1,39 @@
 const $ = (id) => document.getElementById(id);
 
+const showProductInCart = (products, total) => {
+  if ($("cart-table")) {
+    $("cart-table").innerHTML = null;
+    products.forEach(({ id, image, name, price, discount, quantity }) => {
+      $("cart-table").innerHTML += `
+      <tr>
+      <th scope="row"><img src="/images/${image}" width=100 alt="hola">
+      </th>
+      <td>${name}</td>
+      <td>
+        <div class="d-flex gap-2">
+          <button onclick="removeItemToCart(${id})" class="btn btn-sm btn-danger"><i class="fas fa-minus"></i></button>
+          <input type="text" value="${quantity}" style="width:30px;text-align:center">
+          <button onclick="addItemToCart(1, ${id})" class="btn btn-sm btn-success"><i class="fas fa-plus"></i></button>
+        </div>
+      </td>
+      <td>${price * quantity}</td>
+      <td>
+        <h3 class="text-danger" onclick="removeAllItem(${id})"><i class="fa fa-trash"></i></h3>
+      </td>
+    </tr>
+      `;
+    });
+    $("showTotal").innerHTML = total;
+  }
+};
+
 const addItemToCart = async (quantity, product) => {
   try {
     const response = await fetch("/cart", {
       method: "POST",
       body: JSON.stringify({
         quantity,
-        product,
+        product: +product,
       }),
       headers: {
         "Content-Type": "application/json",
@@ -18,7 +45,48 @@ const addItemToCart = async (quantity, product) => {
     if (!ok) {
       throw new Error(message);
     }
+    showProductInCart(cart.products, cart.total);
+  } catch (error) {
+    Swal.fire({
+      title: "Upss",
+      text: error.message,
+      icon: "error",
+    });
+  }
+};
 
+const removeItemToCart = async (id) => {
+  try {
+    const response = await fetch(`/cart?product=${id}`, {
+      method: "DELETE",
+    });
+
+    const { ok, cart, message } = await response.json();
+
+    if (!ok) {
+      throw new Error(message);
+    }
+
+    showProductInCart(cart.products, cart.total);
+  } catch (error) {
+    Swal.fire({
+      title: "Upss",
+      text: error.message,
+      icon: "error",
+    });
+  }
+};
+
+const removeAllItem = async (id) => {
+  try {
+    const response = await fetch(`/cart/item-all?product=${id}`, {
+      method: "DELETE",
+    });
+    const result = await response.json();
+
+    if (!ok) {
+      throw new Error(message);
+    }
   } catch (error) {
     Swal.fire({
       title: "Upss",
@@ -36,9 +104,7 @@ window.onload = function () {
         const { ok, cart, message } = await response.json();
         if (ok) {
           if (cart.products.length) {
-            $(
-              "cart-body"
-            ).innerHTML = `
+            $("cart-body").innerHTML = `
             <table class="table">
               <thead>
                 <tr>
@@ -51,19 +117,14 @@ window.onload = function () {
               <tbody id="cart-table">
                            
               </tbody>
+              <caption>
+              <div class="d-flex justify-content-end">
+                $ <span id="showTotal"></span>
+              </div>
+              </caption>
             </table>
-            `;          
-            cart.products.forEach(({image, name, price, discount, quantity}) => {
-              $('cart-table').innerHTML += `
-              <tr>
-              <th scope="row"><img src="/images/${image}" width=100 alt="">
-              </th>
-              <td>${name}</td>
-              <td>${quantity}</td>
-              <td>${price}</td>
-            </tr>
-              `
-            });
+            `;
+            showProductInCart(cart.products, cart.total);
           } else {
             $(
               "cart-body"
